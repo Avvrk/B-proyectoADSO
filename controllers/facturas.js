3f269c768d6953d7ba3ab40d868e46d2a605e91c
Vyktor23 <ramirezrangelvictor2@gmail.com>
2024-08-22 07:11:32 -0500

    hi

--- controllers/facturas.js
+++ controllers/facturas.js
@@ -3,7 +3,7 @@ import Factura from "../models/factura.js";
 const httpFacturas = {
     getFacturas: async (req, res) => {
         try {
-            const facturas = await Factura.find().populate("comprador_id", "nombre").populate("inventario_id", "observacion");
+            const facturas = await Factura.find().populate("comprador_id", "nombre").populate("inventario_id", "tipo");
             res.json({ facturas });
         } catch (error) {
             res.json({ error });
