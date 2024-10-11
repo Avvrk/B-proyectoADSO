11bfbf3df618f4886995d4362d89aa441a883843
Avvrk <jomaldonado480@gmail.com>
2024-10-11 18:40:15 -0500

    j c g

--- models/gastos.js
+++ models/gastos.js
@@ -7,24 +7,22 @@ const gastoSchema = new mongoose.Schema({
   numeroFactura: { type: String },
   descripcion: { type: String },
   total: { type: Number, required: true },
-  
   insumos: [
     {
       Id_proveedor: { type: mongoose.Schema.Types.ObjectId, ref: 'proveedor' },
       Id_insumos: { type: mongoose.Schema.Types.ObjectId, ref: 'Insumo' },
-      unidad: { type: Number, required: true },
-      total: { type: Number, required: true },
-      cantidad: { type: Number, required: true }
+      unidad: { type: Number },
+      total: { type: Number },
+      cantidad: { type: Number }
     }
   ],
-
   semillas: [
     {
       Id_proveedor: { type: mongoose.Schema.Types.ObjectId, ref: 'proveedor' },
       Id_semilla: { type: mongoose.Schema.Types.ObjectId, ref: 'semilla' },
-      unidad: { type: Number, required: true },
-      total: { type: Number, required: true },
-      cantidad: { type: Number, required: true }
+      unidad: { type: Number },
+      total: { type: Number },
+      cantidad: { type: Number }
     }
   ],
 
